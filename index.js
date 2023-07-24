698e464dc5fd5b94a695455c2e0222d8121f2205
Sanisinh <esparkbiz.sanisinh@gmail.com>
2023-07-24 14:54:11 +0530

    setup done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
-
 import { AppRegistry } from 'react-native';
-import App from './App';
 import { name as appName } from './app.json';
+import { App } from './src/app';
 
 AppRegistry.registerComponent(appName, () => App);
