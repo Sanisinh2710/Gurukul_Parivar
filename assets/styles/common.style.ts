f91b603b020c444820d472bd935a5be687c9d2a2
Sumit-Bhardwaj <sumit.bhardwaj.eSparkbiz@gmail.com>
2023-07-24 14:41:08 +0530

    configuring project

---
+++ assets/styles/common.style.ts
@@ -0,0 +1,5 @@
+import {StyleSheet} from 'react-native';
+
+export const CommonStyle = () => {
+  return StyleSheet.create({});
+};
---
+++ src/app/App.tsx
@@ -0,0 +1,5 @@
+import {Text} from 'react-native';
+
+export const App = () => {
+  return <Text>App Running</Text>;
+};
