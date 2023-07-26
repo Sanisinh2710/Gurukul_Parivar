20e66d747f5ad32bc3405113233ca99995dab7e9
Sanisinh <esparkbiz.sanisinh@gmail.com>
2023-07-26 19:07:23 +0530

    changes in some component

--- src/components/features/OTP/OTPComponent.tsx
+++ src/components/features/OTP/OTPComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ScrollView, TextInput, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {COLORS} from '../../../utils/colors';
+import {COLORS} from '../../../utils';
 import {useOtpStyle} from './otpStyle';
 
 type otpTypeProps = {
@@ -28,51 +28,47 @@ export const OtpComponent = React.memo(({num, setNum}: otpTypeProps) => {
   }, [num]);
 
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={{justifyContent: 'center', gap: 10, flexDirection: 'row'}}>
-          <View style={style.otpWrapper}>
-            {num.map((item, index) => (
-              <TextInput
-                key={index}
-                onSubmitEditing={() => {
-                  return (
-                    num[index] === '' &&
-                    refs.current[index]?.setNativeProps({
-                      style: {borderColor: 'rgba(172, 43, 49, 0.18)'},
-                    })
-                  );
-                }}
-                onFocus={() => {
-                  setFocus(prev => !prev);
-                }}
-                onChangeText={val => {
-                  const newOtpValue = [...num];
-                  newOtpValue[index] = val;
-                  setNum(newOtpValue);
-                }}
-                keyboardType="number-pad"
-                ref={el => (refs.current[index] = el)}
-                style={
-                  refs.current[index]?.isFocused()
-                    ? [style.textInput, {borderColor: COLORS.primaryColor}]
-                    : [style.textInput]
-                }
-                onKeyPress={({nativeEvent}) => {
-                  if (nativeEvent.key === 'Backspace') {
-                    changeFocus(index - 1);
-                    setFocus(false);
-                  } else {
-                    changeFocus(index + 1);
-                    setFocus(false);
-                  }
-                }}
-                maxLength={1}
-              />
-            ))}
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    s<View style={{justifyContent: 'center', gap: 10, flexDirection: 'row'}}>
+      <View style={style.otpWrapper}>
+        {num.map((item, index) => (
+          <TextInput
+            key={index}
+            onSubmitEditing={() => {
+              return (
+                num[index] === '' &&
+                refs.current[index]?.setNativeProps({
+                  style: {borderColor: 'rgba(172, 43, 49, 0.18)'},
+                })
+              );
+            }}
+            onFocus={() => {
+              setFocus(prev => !prev);
+            }}
+            onChangeText={val => {
+              const newOtpValue = [...num];
+              newOtpValue[index] = val;
+              setNum(newOtpValue);
+            }}
+            keyboardType="number-pad"
+            ref={el => (refs.current[index] = el)}
+            style={
+              refs.current[index]?.isFocused()
+                ? [style.textInput, {borderColor: COLORS.primaryColor}]
+                : [style.textInput]
+            }
+            onKeyPress={({nativeEvent}) => {
+              if (nativeEvent.key === 'Backspace') {
+                changeFocus(index - 1);
+                setFocus(false);
+              } else {
+                changeFocus(index + 1);
+                setFocus(false);
+              }
+            }}
+            maxLength={1}
+          />
+        ))}
+      </View>
+    </View>
   );
 });
